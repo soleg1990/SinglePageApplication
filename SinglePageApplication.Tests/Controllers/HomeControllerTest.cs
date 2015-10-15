9a04acb7a7e9aee4a426283bf215a21f7d8aaf34
soleg1990 <soleg1990@gmail.com>
2015-10-15 15:40:35 +0400

    Добавлен график загруженности. Данные берутся с помощью ajax-запроса(get) к методу GetPercentOfCPULoad HomeController'а.

---
+++ SinglePageApplication.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SinglePageApplication;
+using SinglePageApplication.Controllers;
+
+namespace SinglePageApplication.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void PercentOfCPULoadIsReturns()
+        {
+            HomeController controller = new HomeController();
+
+            float percent;
+            float.TryParse(controller.GetPercentOfCPULoad().Data.ToString(), out percent);
+
+            Assert.IsTrue((percent > 0 && percent < 100));
+        }
+
+    }
+}
---
+++ SinglePageApplication/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SinglePageApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetPercentOfCPULoad()
+        {
+            System.Diagnostics.PerformanceCounter perfomanceCounter = new System.Diagnostics.PerformanceCounter();
+            perfomanceCounter.CategoryName = "Processor";
+            perfomanceCounter.CounterName = "% Processor Time";
+            perfomanceCounter.InstanceName = "_Total";
+            float raw = 0;
+            while ((raw == 0 || raw == 100))
+            {
+                raw = perfomanceCounter.NextValue();
+            }
+            return Json(raw, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
