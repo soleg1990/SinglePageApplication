d622a8760956d91e7a257be4f69c6bd50df7a569
soleg1990 <soleg1990@gmail.com>
2015-10-15 18:26:34 +0400

    Исправлена проблема с toolTip'ом spirkline (добавлены стили). Скрипты разнесены по папкам.

--- SinglePageApplication/App_Start/BundleConfig.cs
+++ SinglePageApplication/App_Start/BundleConfig.cs
@@ -9,21 +9,21 @@ namespace SinglePageApplication
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                         "~/Scripts/jquery-1.10.2.min.js"));
+                         "~/Scripts/Libs/jquery-1.10.2.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/sparkline").Include("~/Scripts/jquery.sparkline.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/sparkline").Include("~/Scripts/Libs/sparkline.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/Libs/jquery.validate*"));
 
             // Используйте версию Modernizr для разработчиков, чтобы учиться работать. Когда вы будете готовы перейти к работе,
             // используйте средство сборки на сайте http://modernizr.com, чтобы выбрать только нужные тесты.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/Libs/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/Libs/bootstrap.js",
+                      "~/Scripts/Libs/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
